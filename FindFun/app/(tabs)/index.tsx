baffc5a1f2cbff02a61da5a4c5da59fdf69ed28b
ZachVandecar <99353525+ZachVandecar@users.noreply.github.com>
2024-10-01 13:46:38 -0400

    Test Foursquare API Implemented
    There is a test function that returns a static result. I am working on how we can change the fetch dynamically so there are parts of that in there.

--- FindFun/app/(tabs)/index.tsx
+++ FindFun/app/(tabs)/index.tsx
@@ -1,10 +1,99 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, ImageBackground, StyleSheet, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+
+const API_KEY = 'fsq3VU8AXCRv6QBKaBi5+EcmNWdypVrGRPvE/cqh4nxz4p4='
+
 
 const WelcomeScreen = () => {
   const navigation = useNavigation();
   const [searchQuery, setSearchQuery] = useState('');
+
+
+  async function foursquareTest()
+  {
+    try {
+
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: API_KEY
+      }
+
+    };
+    
+
+
+
+    const base_URL = 'https://api.foursquare.com/v3/places/search';
+    var final_URL = 'finalURL';
+
+
+
+    //if the variable is active, make it like these below:
+    var query = '?query='; //A string to be matched against all content for this place, including but not limited to venue name, category, telephone number, taste, and tips.
+    var ll = '&ll=';
+    var radius = '&radius=';  
+    var categories = '&categories='; //Filters the response and returns FSQ Places matching the specified categories. Supports multiple Category IDs, separated by commas.
+    var fields = '&fields=';
+    var min_price = '&min_price=';
+    var max_price = '&max_price=';
+    var open_now = '&open_now=';
+    var near = '&near=';
+    var sort = '&sort=';
+    var limit = '&limit=';
+
+    //if the variable is not asked for by search, it can just be empty:
+    var query = ''; 
+    var ll = '';
+    var radius = '';  
+    var categories = ''; 
+    var fields = '';
+    var min_price = '';
+    var max_price = '';
+    var open_now = '';
+    var near = '';
+    var sort = '';
+    var limit = '';
+
+
+
+    var exampleURL = 'https://api.foursquare.com/v3/places/search?query=query&ll=41.8781%2C-87.6298&radius=10&categories=29348&fields=social_media%2C%20website&min_price=1&max_price=4&open_now=true&near=Chicago%2C%20IL&sort=RELEVANCE&limit=10'
+
+    final_URL = base_URL + query + ll + radius + categories + fields + min_price + max_price + open_now + near + sort + limit;
+
+
+
+
+
+    final_URL = 'https://api.foursquare.com/v3/places/search?near=Chicago%2C%20IL&sort=RELEVANCE&'; //test search that works guarantee
+
+
+    const response = await fetch(final_URL, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+      
+
+
+    } catch (error) {
+
+      console.error('Error fetching data from Foursquare:', error);
+
+    }
+
+  }
+
+  useEffect(() => {
+
+    foursquareTest();
+
+
+  },[]);
 
   return (
     <ImageBackground
