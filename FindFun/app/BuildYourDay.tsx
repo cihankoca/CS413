69627812ffed69b182a43bc933ff3d9c1a538ade
ZachVandecar <99353525+ZachVandecar@users.noreply.github.com>
2024-11-05 01:00:54 -0500

    Foursquare for Make Your Day, with bugs
    All that needs to be done, is filling in the prompt with the data we actually want to send to gpt (in a good format for it). The bug though, is that the geocoding will always take string vars but not user input for some reason. Will try to fix later.

--- FindFun/app/BuildYourDay.tsx
+++ FindFun/app/BuildYourDay.tsx
@@ -4,10 +4,10 @@ import { View, Text, TouchableOpacity, TextInput, ScrollView, StyleSheet, Dimens
 const buildPageBackground = require('../assets/images/buildpage.png');
 
 const OPENAI_API_KEY = process.env.EXPO_PUBLIC_OPENAI_API_KEY;
-
+const Geocode_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_GEOENCODING_API_KEY; 
 const FOURSQUARE_API_KEY = process.env.EXPO_PUBLIC_FOURSQUARE_API_KEY;
 
-interface Place {
+interface Place { //maybe unnecessary...I just need a good way to give all json results to gpt in an organized way
     name: string;
     address: string;
     latitude: number;
@@ -15,18 +15,112 @@ interface Place {
     hours: string | null;
     website: string | null;
     rating: number | null;
+
   }
-  function extractPlaces(data: any): Place[] {
+  
+interface SearchParams { //basically a struct for search parameters. create one and change it as user messes with options/search. Then pass it to the function and a search will be made with these params
+    query?: string;
+    ll?: string; // latitude,longitude  /required
+    radius?: number;
+    categories?: string; // comma-separated category IDs. 
+    fields?: string; // comma-separated fields
+    min_price?: number;
+    max_price?: number;
+    open_now?: boolean;
+    near?: string;
+    sort?: string;
+    limit?: number;
+    exclude_all_chains?: boolean;
+  
+  }
+
+  function extractPlaceInfo(data: any): Place[] {
     return data.results.map((place: any) => ({
-      name: place.name,
-      address: place.location.formatted_address || place.location.address || "Address not available",
-      latitude: place.geocodes.main.latitude,
-      longitude: place.geocodes.main.longitude,
-      hours: place.hours ? place.hours.display : null,
-      website: place.website || null,
-      rating: place.rating || null
+        name: place.name,
+        address: place.location?.formatted_address || place.location?.address || "Address not available",
+        latitude: place.geocodes?.main?.latitude || 0, // defaulting to 0 if undefined
+        longitude: place.geocodes?.main?.longitude || 0,
+        hours: place.hours?.display || "Hours not available",
+        website: place.website || "Website not available",
+        rating: place.rating || "Rating not available"
     }));
+}
+
+  async function getCoordinates(address: string)
+  {
+
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${Geocode_API_KEY}`);
+    
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    if (data.status === 'OK') {
+        const { lat, lng } = data.results[0].geometry.location;
+        return { latitude: lat, longitude: lng };
+    } else {
+        throw new Error('Geocoding failed: ' + data.status);
+    }
+}
+
+async function placesSearch(params: SearchParams)
+{
+  try {
+
+
+  const options: RequestInit = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      ...(FOURSQUARE_API_KEY ? { Authorization: FOURSQUARE_API_KEY } : {})
+    }
+
+  };
+
+  
+  const base_URL = 'https://api.foursquare.com/v3/places/search';
+
+  const urlParams = new URLSearchParams();
+
+  if (params.query) urlParams.append('query', params.query);
+  if (params.ll) urlParams.append('ll', params.ll);
+  if (params.radius) urlParams.append('radius', params.radius.toString());
+  if (params.categories) urlParams.append('categories', params.categories);
+  if (params.fields) urlParams.append('fields', params.fields);
+  if (params.min_price !== undefined) urlParams.append('min_price', params.min_price.toString());
+  if (params.max_price !== undefined) urlParams.append('max_price', params.max_price.toString());
+  if (params.open_now) urlParams.append('open_now', params.open_now.toString());
+  if (params.near) urlParams.append('near', params.near);
+  if (params.sort) urlParams.append('sort', params.sort);
+  if (params.limit) urlParams.append('limit', params.limit.toString());
+  if (params.exclude_all_chains) urlParams.append('exclude_all_chains', params.exclude_all_chains.toString());
+
+
+
+  const final_URL = `${base_URL}?${urlParams.toString()}`;
+
+  const response = await fetch(final_URL, options);
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`);
   }
+
+  const data = await response.json();
+  return data;
+
+  //const response = await fetch(final_URL, options)
+    //.then(response => response.json())
+    //.then(response => console.log(response))
+    //.catch(err => console.error(err));
+
+  } catch (error) {
+
+    console.error('Error fetching data from Foursquare:', error);
+
+  }
+
+}
 
 
 
@@ -92,9 +186,12 @@ const BuildYourDay: React.FC = () => {
 
 
         
-        let tripLocation: string = '';
-        let tripGuidelines: string = '';
-        let tripLength: string = '';
+        let tripLocation: string = "";
+        let tripGuidelines: string = "";
+        let tripLength: string = "";
+
+        let tripLat: any;
+        let tripLong: any;
         
         let jsonResponse: any;
         let placesList: any;
@@ -102,21 +199,50 @@ const BuildYourDay: React.FC = () => {
 
 
         if (step === 1) {
+            tripLocation = `${inputText}`; //is this the issue? I really think it is....
             console.log(inputText); //use inputText to find a latitude and longitude using geocoding
-            tripLocation = inputText;
 
             aiResponse = 'What type of places do you want to explore today?';
             setStep(2); 
         } else if (step === 2) {
             //do a foursquare request using their activities and chosen location...
-            tripGuidelines = inputText;
+            tripGuidelines = `${inputText}`; //this works as well
+            let userAddress = "1600 Amphitheatre Parkway, Mountain View, CA"; // This can come from a user input (can be imperfect...havent tested thoroughly though)
+            userAddress = "Boston"; //this works too...
+            await getCoordinates(userAddress)  //can't get it to work with other than the example string(s)
+            .then(coords => {
+                tripLat = coords.latitude;
+                tripLong = coords.longitude;
+                console.log("Latitude:", tripLat, "Longitude:", tripLong);
+            })
+            .catch(error => console.error(error));
+                
+
+            let search: SearchParams = {};
+            search.ll = `${tripLat},${tripLong}`; //using geocoded lat/long
+            search.radius = 100000; //max radius for testing
+            search.query = tripGuidelines; //this might suck (not work at all)...if so we need a way for user to clearly choose foursquare categories
+            search.limit = 5; //limit of 5 for testing
+            search.exclude_all_chains = true;
+            search.fields = "name,location,description,website,hours,rating,tips";
+
+
+            jsonResponse = await placesSearch(search); //saving the json response from a foursquare search
+            //app gets stuck somewhere around here....the Places search is complete and outputted, so maybe the issue is extract?...fixed, returning was brokey
+            placesList = extractPlaceInfo(jsonResponse); //taking the important bits out to send to gpt
+            console.log("\n THE JSON RESPONSE IS: \n");
+            console.log(jsonResponse);
+            //console.log("\n THE PLACES LIST IS: \n");    //basically just trying to limit tokens given to gpt, but it hasnt mattered much so far...i jsut want to gmake sure gpt understands what attributes belong to which place
+            //console.log(placesList);
+
+            
 
             aiResponse = `Great! You chose: ${inputText}. How long do you have to explore?`;
             setStep(3);
         } else if (step === 3) {
             tripLength = inputText;
-
-            aiResponse = await fetchAIResponse(`Build a day trip itinerary for exploring ${inputText}.`);
+            //send all results to gpt and ask them to make a schedule using the results (maybe add more results too like restaurants or popular stuff if user asks for long schedule or doesn't give enough to work with to fill time)
+            aiResponse = await fetchAIResponse(`Build a day trip itinerary for exploring ${inputText}.`); //this will change a bunch....
             setStep(4);
         }
 
